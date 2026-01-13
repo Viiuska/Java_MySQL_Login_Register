1eee9a8755298cca92ae4b145aae1b48ce6da46f
Viiuska <87257685+Viiuska@users.noreply.github.com>
2026-01-13 15:40:00 +0200

    Username, password GUI added

---
+++ src/constants/CommonConstants.java
@@ -0,0 +1,9 @@
+package constants;
+
+import java.awt.*;
+
+public class CommonConstants {
+    public static final Color PRIMARY_COLOR = Color.decode("#9381ff");
+    public static final Color SECONDARY_COLOR = Color.decode("#b8b8ff");
+    public static final Color TEXT_COLOR = Color.decode("#f8f7ff");
+}
--- src/guis/LoginFormGUI.java
+++ src/guis/LoginFormGUI.java
@@ -1,10 +1,54 @@
 package guis;
 
+import constants.CommonConstants;
+
+import javax.swing.*;
+import java.awt.*;
+
 public class LoginFormGUI extends Form{
     public LoginFormGUI(){
         super("Login");
+        addGUIComponent();
 
 
+    }
+
+    private void addGUIComponent(){
+        JLabel loginLabel = new JLabel("Login");
+        loginLabel.setBounds(0,25,520,100);
+        loginLabel.setForeground(CommonConstants.TEXT_COLOR);
+        loginLabel.setFont(new Font("Helvetica", Font.BOLD, 40));
+        loginLabel.setHorizontalAlignment(SwingConstants.CENTER);
+
+        add(loginLabel);
+
+        JLabel usernameLabel = new JLabel("Username: ");
+        usernameLabel.setBounds(30,150,400,25);
+        usernameLabel.setForeground(CommonConstants.TEXT_COLOR);
+        usernameLabel.setFont(new Font("Helvetica", Font.PLAIN, 18 ));
+
+        JTextField usernameField = new JTextField();
+        usernameField.setBounds(30,180,450,55);
+        usernameField.setBackground(CommonConstants.SECONDARY_COLOR);
+        usernameField.setForeground(CommonConstants.TEXT_COLOR);
+        usernameField.setFont(new Font("Helvetica", Font.PLAIN, 24 ));
+
+        add(usernameLabel);
+        add(usernameField);
+
+        JLabel passwordLabel = new JLabel("Password: ");
+        passwordLabel.setBounds(30,335,450,55);
+        passwordLabel.setForeground(CommonConstants.TEXT_COLOR);
+        passwordLabel.setFont(new Font("Helvetica", Font.PLAIN, 18 ));
+
+        JPasswordField passwordField = new JPasswordField();
+        passwordField.setBounds(30,380,450,55);
+        passwordField.setBackground(CommonConstants.SECONDARY_COLOR);
+        passwordField.setForeground(CommonConstants.TEXT_COLOR);
+        passwordField.setFont(new Font("Helvetica", Font.PLAIN, 24 ));
+
+        add(passwordLabel);
+        add(passwordField);
 
     }
 }
